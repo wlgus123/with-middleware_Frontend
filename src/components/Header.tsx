1c6466d3740416d6b5ed2c4ac15e6e8dea356f52
iris-starry <iris_starry_@naver.com>
2024-06-04 16:36:16 +0900

    feat: 사이드바

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,13 @@ const Header: React.FC = () => {
     return (
         <div className="header">
             {/* 왼쪽 로고 이미지 */}
-            <img src="/Logo.svg" alt="로고" className="logo" />
+            <img src="/images/header/Logo.svg" alt="로고" className="logo" />
 
             {/* 오른쪽 아이콘 */}
             <div className="icons">
-                <img src="/icon/search_icon.svg" alt="검색 아이콘" className="icon" />
-                <img src="/icon/alarm_icon.svg" alt="알람 아이콘" className="icon" />
+                <img src="/images/header/search_icon.svg" alt="검색 아이콘" className="icon"/>
+                <img src="/images/header/user_icon.svg" alt="유저 아이콘" className="icon"/>
+                <img src="/images/header/alarm_icon.svg" alt="알람 아이콘" className="icon"/>
             </div>
         </div>
     );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,82 @@
-// Sidebar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Sidebar.css';
+import Login from './modal/Login';
 
 const Sidebar: React.FC = () => {
+    const [showLogin, setShowLogin] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const handleLoginClick = () => {
+        setShowLogin(true);
+    };
+
+    const closeLoginModal = () => {
+        setShowLogin(false);
+    };
+
+    const toggleSidebar = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
     return (
-        <aside className="sidebar">
+        <aside className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+            <button className={`toggle-button ${isCollapsed ? 'collapsed' : ''}`} onClick={toggleSidebar}>
+                <img src={`/images/sidebar/${isCollapsed ? 'arrow_right' : 'arrow_left'}.svg`} alt="Toggle Sidebar" />
+            </button>
+            <div className="sidebar-header">
+                <img src="/images/sidebar/default_profile.svg" alt="Profile" className="profile-image" />
+                {!isCollapsed && (
+                    <button className="login-button" onClick={handleLoginClick}>로그인</button>
+                )}
+            </div>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/post">글쓰기</Link></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li>
+                        <Link to="/post">
+                            <img src="/images/sidebar/write_icon.svg" alt="글쓰기 아이콘" className="nav-icon" />
+                            <span className="nav-text">글쓰기</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <img src="/images/sidebar/team_icon.svg" alt="팀 프로젝트 아이콘" className="nav-icon" />
+                            <span className="nav-text">팀 프로젝트</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="/images/sidebar/designer_icon.svg" alt="디자이너 아이콘" className="nav-icon" />
+                            <span className="nav-text">디자이너</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="/images/sidebar/developer_icon.svg" alt="개발자 아이콘" className="nav-icon" />
+                            <span className="nav-text">개발자</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="/images/sidebar/study_icon.svg" alt="스터디 아이콘" className="nav-icon" />
+                            <span className="nav-text">스터디</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="/images/sidebar/heart_icon.svg" alt="찜한 게시물 아이콘" className="nav-icon" />
+                            <span className="nav-text">찜한 게시물</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="/images/sidebar/RecentlyViewed_icon.svg" alt="최근 본 게시물 아이콘" className="nav-icon" />
+                            <span className="nav-text">최근 본 게시물</span>
+                        </a>
+                    </li>
                 </ul>
             </nav>
+            {showLogin && <Login onClose={closeLoginModal} />}
         </aside>
     );
 };
